a4322c6e899af9e1c21e820bea91f570e09f25d9
weixian-nwx <98571158+weixian-nwx@users.noreply.github.com>
2025-04-01 00:27:52 +0800

    added min smoke age, sugar effect

---
+++ js/national_policy.js
@@ -0,0 +1,25 @@
+import { updateSliderLabel } from "./script.js";
+
+var minSmokeAge = 21; // Default minimum smoking age
+
+function updateMinSmokeAge() {
+    minSmokeAge = parseFloat(document.getElementById("min-age").value) || 21;
+    console.log("Minimum smoking age set to: ", retirementAge);
+}
+
+function updateSugarLevel(value) {
+    sugarLevel = parseFloat(value);
+    updateSliderLabel(value, "reco-sugar-value");
+}
+
+function updateSodiumLevel(value) {
+    sodiumLevel = parseFloat(value);
+    updateSliderLabel(value, "reco-sodium-value");
+}
+
+export {
+    minSmokeAge,
+    updateMinSmokeAge,
+    updateSugarLevel,
+    updateSodiumLevel
+};
